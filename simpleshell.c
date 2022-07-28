b4004c8fa0c0a3dbd10112222e9b6dad09af5aa7
wdmd2022 <doug@dougrules.com>
2022-07-28 08:04:11 -0700

    including more files related to shell for initial commit round

---
+++ simpleshell.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * main - get going
+ * Return: 0 on success, 1 otherwise
+ */
+int main(void)
+{
+	char **command;
+	char *buffer = NULL;
+	char leave[] = "exit\n";
+	char vire[] = "env\n";
+	size_t size = 0;
+
+	while (1)
+	{
+		_printf("$ ");
+		getline(&buffer, &size, stdin);
+		if (buffer[0] == '\n')
+		{
+			continue;
+		}
+		if (_strcmp(buffer, leave) == 0)
+			break;
+		if (_strcmp(buffer, vire) == 0)
+		{
+			printenv();
+			continue;
+		}
+		strtok(buffer, " ");
+	}
+	free(buffer);
+	return (0);
+}
+/**
+ * printenv - prints the environment
+ *
+ * Return: nothing
+ */
+void printenv(void)
+{
+	size_t i;
+
+	for (size_t i = 0; environ[i] != NULL; i++)
+	{
+		printf("%s\n", environ[i]);
+	}
+}
